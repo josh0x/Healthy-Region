6ca0eab35db8120a4a3776c1cd8bdadcd350c059
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2021-03-22 11:30:17 +0100

    0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResearcherController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\DocumentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::resource('/news', NewsController::class);
+Route::resource('/researchers', ResearcherController::class);
+//Route::resource('/documents', DocumentController::class);
+Route::get('/documents',[DocumentController::class,'index']);
+Route::post('/documents',[DocumentController::class , 'store']);
+Route::get('/documents/create',[DocumentController::class , 'create']);
+Route::get('/documents/{document}',[DocumentController::class,'show']);
+Route::get('documents/download/{document}',[DocumentController::class,'download']);
