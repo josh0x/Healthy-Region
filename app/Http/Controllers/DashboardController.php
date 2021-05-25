04f527da87303bb0fdf87e776b35357b83d8f6b8
DylanJongepier <jong0314@hz.nl>
2021-05-25 13:20:38 +0200

    Add dashboard search. (Only DD)

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        //Search function DD
+        if (request()->query('query')) {
+            dd(request()->query('query'));
+        }
+    }
+}
