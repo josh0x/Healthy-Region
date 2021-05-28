ac9928177254a7c1dcd1e7f58e5b890a97708963
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2021-05-28 13:05:44 +0200

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,7 +15,6 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        // $document = Document::latest(10)->get();
         $projects = Project::get();
 
         return view('projects.index', ['projects' => $projects]);
@@ -35,20 +34,21 @@ class ProjectController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
+     * @param  \App\Models\Project  $project
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $document = Project::create($this->validateProject($request));
+        $project =Project::create($this->validateProject($request));
+        $project->save();
         return  redirect('projects');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Project  $document
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
     public function show(Project $project)
@@ -59,7 +59,7 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Project  $document
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
     public function edit(Project $project)
@@ -71,10 +71,10 @@ class ProjectController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $document
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Project $project)
+    public function update(Request $request)
     {
         $project->update($this->validateProject($request));
         return redirect($project->path());
@@ -83,7 +83,7 @@ class ProjectController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Project  $document
+     * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
     public function destroy(Project $project)
