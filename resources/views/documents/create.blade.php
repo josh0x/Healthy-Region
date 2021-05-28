740a62ed2ab1f225a5a837a4a725f5fc4775cb90
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2021-05-28 17:58:17 +0200

    Update create.blade.php

--- resources/views/documents/create.blade.php
+++ resources/views/documents/create.blade.php
@@ -7,12 +7,6 @@
     <div class="container mx-auto px-4 py-10 flex justify-center text-black">
         <form action='/documents' class="x-form text-black" method="POST" enctype="multipart/form-data">
             @csrf
-            @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <strong>{{ $message }}</strong>
-            </div>
-          @endif
-
           @if (count($errors) > 0)
             <div class="alert alert-danger">
                 <ul>
@@ -101,6 +95,11 @@
                 </div>
 
             <div class="mt-10 flex items-center justify-center bg-grey-lighter">
+                @if (session()->has('success'))
+                    <div class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        <strong>{{ session()->get('success')}}</strong>
+                    </div>
+                @endif
                     <label class="@error('file') border-red-400 @enderror w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-md shadow-md tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-black">
                         <svg class="w-8 h-8" fill="blue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
