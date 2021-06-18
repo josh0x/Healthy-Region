330e80fe1d91c69a271a95242b8a2f5a55c0ef8b
amjadhwidy <70522055+amjadhwidy@users.noreply.github.com>
2021-06-18 13:35:21 +0200

    Update index.blade.php

--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -61,7 +61,8 @@
                                         <div class="text-sm font-medium text-gray-900">
                                             {{$doc->user->name}}
                                         </div>
-                                    <div class="text-sm text-gray-500">
+                                    </div>
+
                                 </div>
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap">
